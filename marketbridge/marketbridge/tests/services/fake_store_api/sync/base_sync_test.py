be54d5a999aeabcd2afc0d1600e48abca8146913
Willy Tedjakusuma <willy.tedjakusuma@gmail.com>
2025-06-17 11:06:52 +0700

    feat: add base test functionality for sync module

--- marketbridge/marketbridge/tests/services/fake_store_api/sync/base_sync_test.py
+++ marketbridge/marketbridge/tests/services/fake_store_api/sync/base_sync_test.py
@@ -2,16 +2,15 @@ import pytest
 
 from os import environ
 from requests.exceptions import HTTPError
-from marketbridge.services.fake_store_api.sync import sync_entities
+from marketbridge.services.fake_store_api.sync import sync_entity, get_entity_model
 from marketbridge.services.fake_store_api.fetch import FakeStoreAPI
 
 class BaseSyncTest:
     ENTITY = None
-    ENTITY_NAME = None
     ENTITY_DATA = []
     
     def test_success(self, db, monkeypatch):
-        monkeypatch.setattr(FakeStoreAPI, 'fetch', lambda self, entity: self.ENTITY_DATA)
-        sync_entities(self.ENTITY_NAME)
+        monkeypatch.setattr(FakeStoreAPI, 'fetch', lambda _self, _entity: self.ENTITY_DATA)
+        sync_entity(self.ENTITY)
         
-        assert self.ENTITY.objects.count() == len(self.ENTITY_DATA)
+        assert get_entity_model(self.ENTITY).objects.count() == len(self.ENTITY_DATA)
